a730a61a6305fef54e83be0cd631bbaf4b1aed55
Gustavo Morillo <gustavomorillo@gmail.com>
2020-04-26 22:01:22 -0400

    #first

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRouter = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRouter);
+
+const mongoUri = process.env.DB;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(req.user.email);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
